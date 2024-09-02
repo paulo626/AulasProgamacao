9894399efddfff3a63f6780bb99833a318203dee
Paulo.R.F.Pizza <117474750+paulo626@users.noreply.github.com>
2024-09-02 08:45:17 -0300

    Create scriptTets.js

---
+++ Aula1-math/scriptTets.js
@@ -0,0 +1,12 @@
+function exe1(){
+    let numero1 = parseInt(prompt("digite um numero"))
+    let numero2 = parseInt(prompt("digite outro numero"))
+
+
+    console.log(Math.round(numero))
+
+    console.log(Math.ceil(numero2))
+
+    console.log(Math.floor(numero2))
+
+}
