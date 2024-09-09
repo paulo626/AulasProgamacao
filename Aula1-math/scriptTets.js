02e8a5d77db072336e780c3c3b48a0f5fdc8345d
Paulo.R.F.Pizza <117474750+paulo626@users.noreply.github.com>
2024-09-09 12:47:04 -0300

    Update scriptTets.js

--- Aula1-math/scriptTets.js
+++ Aula1-math/scriptTets.js
@@ -14,13 +14,13 @@ function exe2(){
 function exe3(){
     let numero1 = parseInt(prompt("digite um numero"))
     let numero2 = parseInt(prompt("digite um outro"))
-    let numero3 = parseInt(prompt("digite um numero"))
+    let numero3 = parseInt(prompt("digite um numero"))      
     let numero4 = parseInt(prompt("digite um outro"))
     alert(`O menor numero é ${Math.min(numero1,numero2,numero3,numero4,)}`)
-    // 3 ; retorna o menor numero escrito
+    //retorna o menor numero escrito
 
     alert(`O maior numero é ${Math.max(numero1,numero2,numero3,numero4,)}`)
-    // 632 ; retorna o maior numero escrito
+    //retorna o maior numero escrito
 
 }
 
